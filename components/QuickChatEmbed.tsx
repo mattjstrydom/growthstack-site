bbfc2e74ee9bad35697a2443d1c30a1d99fbfb2a
mattvisme <tstrydom@gmail.com>
2026-04-21 10:29:58 +0200

    Create QuickChatEmbed.tsx

---
+++ components/QuickChatEmbed.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    dataLayer?: Record<string, unknown>[];
+  }
+}
+
+interface QuickChatEmbedProps {
+  calLink: string;
+}
+
+export default function QuickChatEmbed({ calLink }: QuickChatEmbedProps) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'quick_chat_page_view',
+      page_path: window.location.pathname,
+      cal_link: calLink,
+    });
+  }, [calLink]);
+
+  const handleLoad = () => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'quick_chat_calendar_load',
+      page_path: window.location.pathname,
+      cal_link: calLink,
+    });
+  };
+
+  return (
+    <iframe
+      src={calLink}
+      title="Book a quick chat with GrowthStack"
+      style={{
+        width: '100%',
+        minHeight: '920px',
+        border: '0',
+        borderRadius: '14px',
+        background: '#fff',
+      }}
+      loading="lazy"
+      onLoad={handleLoad}
+    />
+  );
+}
